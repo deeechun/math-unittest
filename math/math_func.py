4597b3682274496619bcf5f3df75e8dde4606bdf
David <dechun92@gmail.com>
2016-08-15 17:44:49 -0700

    Add in docstrings

--- math/math_func.py
+++ math/math_func.py
@@ -1,12 +1,22 @@
+
+"""Simple math functions"""
+"""These functions should only be used with number types"""
+
+"""Takes in two arguments and returns the addition of the two"""
 def add(x, y):
     return x + y
 
+"""Takes in two arguments and returns the multipication of the two"""
 def multiply(x, y):
     return x * y
 
+"""Takes in two arguments and returns the division of the first argument by the second argument"""
+"""For example, divide(6,3) will calculate '6 / 3' and return 2"""
 def divide(x, y):
     return x / y
 
+"""Takes in two arguments and returns the subtraction of the second argument from the first argument"""
+"""For example, subtract(1,2) will calculate '1 - 2' and return -1"""
 def subtract(x, y):
     return x - y
 
--- math/test_math_func.py
+++ math/test_math_func.py
@@ -1,4 +1,6 @@
 import unittest
+
+"""These import the functions from math_func in the same directory"""
 from math_func import (
 	add,
 	subtract,
@@ -6,29 +8,34 @@ from math_func import (
 	divide
 	)
 
-#Create a class that inherits from TestCase class from unittest package
+
 class Test_math_func(unittest.TestCase):
 
+	"""This test uses two arguments (2 and 3) in the add function and expects to see an output of 5"""
     def test_add(self):
     	added_value = add(2,3)
     	expected_value = 5
     	self.assertEqual(added_value, expected_value)
 
+    """This test uses two arguments (2 and 3) in the subtract function and expects to see an output of -1"""
     def test_subtract(self):
     	subtracted_value = subtract(2,3)
     	expected_value = -1
     	self.assertEqual(subtracted_value, expected_value)
 
+    """This test uses two arguments (2 and 3) in the multiply function and expects to see an output of 6"""
     def test_multiply(self):
     	multiplied_value = multiply(2,3)
     	expected_value = 6
     	self.assertEqual(multiplied_value, expected_value)
 
+    """This test uses two arguments (4 and 2) in the divide function and expects to see an output of 2"""
     def test_divide(self):
     	divided_value = divide(4,2)
     	expected_value = 2
     	self.assertEqual(divided_value, expected_value)
 
-#Only run if ran, not imported
+"""This part of the file will only run if run as a script, not as an import"""
+"""This command will run the unit tests present in this file"""
 if __name__ == '__main__':
 	unittest.main()
