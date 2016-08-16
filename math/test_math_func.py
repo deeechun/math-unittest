50d3703519a8dfdb214fbc20d0d9e251e6f5c503
David <dechun92@gmail.com>
2016-08-15 17:11:19 -0700

    first commit

---
+++ math/test_math_func.py
@@ -0,0 +1,33 @@
+import unittest
+from math_func import (
+	add,
+	subtract,
+	multiply,
+	divide
+	)
+
+
+class Test_math_func(unittest.TestCase):
+
+    def test_add(self):
+    	added_value = add(2,3)
+    	expected_value = 5
+    	self.assertEqual(added_value, expected_value)
+
+    def test_subtract(self):
+    	subtracted_value = subtract(2,3)
+    	expected_value = -1
+    	self.assertEqual(subtracted_value, expected_value)
+
+    def test_multiply(self):
+    	multiplied_value = multiply(2,3)
+    	expected_value = 6
+    	self.assertEqual(multiplied_value, expected_value)
+
+    def test_divide(self):
+    	divided_value = divide(4,2)
+    	expected_value = 2
+    	self.assertEqual(divided_value, expected_value)
+
+if __name__ == '__main__':
+	unittest.main()
