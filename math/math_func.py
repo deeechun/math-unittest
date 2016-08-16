37336c7ef80a3dfdf9025d45f830e08e74e835b6
Chris Morales <lambda.designs.cm@gmail.com>
2016-08-15 18:07:06 -0700

    Added docstrings to describe more in detail what each variable should be as well as the expected behavior for edge cases that were thought of for division.

--- math/math_func.py
+++ math/math_func.py
@@ -1,22 +1,64 @@
+"""
+	Simple math functions. These functions should only be used with number types
+"""
 
-"""Simple math functions"""
-"""These functions should only be used with number types"""
-
-"""Takes in two arguments and returns the addition of the two"""
 def add(x, y):
+"""
+	Takes in two arguments and returns the addition of the two
+
+	Args:
+		x is any real whole positive integer.
+		y is any real whole positive integer.
+
+	Returns:
+		The sum of both x and y.
+"""
     return x + y
 
-"""Takes in two arguments and returns the multipication of the two"""
 def multiply(x, y):
+"""
+	Takes in two arguments and returns the multipication of the two
+
+	Args:
+		x is any real floating number.
+		y is any real floating number.
+
+	Returns:
+		Multiplies both x and y.
+"""
     return x * y
 
-"""Takes in two arguments and returns the division of the first argument by the second argument"""
-"""For example, divide(6,3) will calculate '6 / 3' and return 2"""
 def divide(x, y):
+"""
+	Takes in two arguments and returns the division of the 
+	first argument by the second argument. For example, divide(6,3) 
+	will calculate '6 / 3' and return 2
+
+	Args:
+		x is any real floating number.
+		y is any real floating number except for zero.
+
+	Note:
+		if y is a zero python throws an exception and we will
+		print our own message notifying the user as "undefined answer".
+
+	Returns:
+		Any floating point value.
+"""
     return x / y
 
-"""Takes in two arguments and returns the subtraction of the second argument from the first argument"""
-"""For example, subtract(1,2) will calculate '1 - 2' and return -1"""
 def subtract(x, y):
+"""
+	Takes in two arguments and returns the subtraction of the second
+	argument from the first argument. For example, subtract(1,2) will
+	calculate '1 - 2' and return -1
+
+	Args:
+		x is any real floating number.
+		y is any real floating number.
+
+	Returns:
+		Either a positive or negative floating number.
+"""
     return x - y
 
